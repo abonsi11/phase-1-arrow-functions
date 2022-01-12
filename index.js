3b2ebc8437a105272ed7f16e03ffdaee16ff8e91
Anthony Bonsi <xbonsi96@gmail.com>
2022-01-11 17:35:23 -0700

    commit tree b2aea0725a7b6a0ce94eb0c07ecc704ea9f745ea

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function() {
+    return 2000 / 100
+}
+const square = (x) => x * x
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
+add(3,4)
